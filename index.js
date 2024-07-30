2c453cc7065e44154a3d22312ca75af4881fcf77
JP-Parent <jeanphilippe.parent@gmail.com>
2024-07-30 13:05:11 -0400

    added all functions to make calc work, problem with display more than 1 number after operator

--- index.js
+++ index.js
@@ -1,60 +1,85 @@
-var number1 = 0;
-var number2 = 0;
-var operator = "";
+const display = document.querySelector(".display");
+const digitButtons = document.querySelectorAll(".digit");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector(".misc");
+const equalButton = document.querySelector("#result");
 
-function add(x, y) {
-    return x + y;
+var currentOperand = "";
+var previousOperand = "";
+var operator = null;
+display.innerText = "0";
+
+clearButton.addEventListener("click", allClear);
+equalButton.addEventListener("click", operate);
+
+digitButtons.forEach(digit => {
+    digit.addEventListener("click", () => {
+        appendDigit(digit.value);
+    });
+});
+
+operatorButtons.forEach(opButton => {
+    opButton.addEventListener("click", () => {
+        setOperation(opButton.value)
+    });
+});
+
+function allClear() {
+    display.textContent = '0';
+    currentOperand = '';
+    previousOperand = '';
+    operator = null;
 }
 
-function substract(x, y) {
-    return x - y;
-}
-
-function multiply(x, y) {
-    return x * y;
-}
-
-function divide(x, y) {
-    return x / y;
-}
-
-function operate(x, y, sign) {
-    switch (sign) {
-        case "+":
-            add(x, y)
-            break;
-        case "-":
-            substract(x, y)
-            break;
-        case "*":
-            multiply(x, y)
-            break;
-        case "/":
-            divide(x, y)
-            break;
-        default:
-            return "Error";
+function appendDigit(digit) {
+    if (display.textContent.length >= 11) return;
+    if (display.textContent === '0' || previousOperand !== "") {
+        display.textContent = digit;
+    } else {
+        display.textContent += digit;
     }
 }
 
-
-var digitButtons = document.querySelector(".digits");
-for (let i = 0; i <= 9; i++) {
-    var digit = document.createElement("button");
-    digit.innerText = i;
-    digitButtons.appendChild(digit);
+function setOperation(op){
+    if(currentOperand !== ""){
+        operate();
+    }
+    if(previousOperand !== "" && operator !== "="){
+        operate();
+    }
+        operator = op;
+        previousOperand = display.innerText;
+        currentOperand = "";
 }
 
-
-const operators = ["+", "-", "*", "/", "="];
-var operatorButtons = document.querySelector(".operators");
-for (let i = 0; i < operators.length; i++) {
-    var operator = document.createElement("button");
-    operator.innerText = operators[i];
-    operatorButtons.appendChild(operator);
+function operate() {
+    currentOperand = display.innerText;
+    if (operator === null || currentOperand === '') return;
+    let result;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    switch (operator) {
+        case "+":
+            result = prev + current;
+            break;
+        case "-":
+            result = prev - current;
+            break;
+        case "*":
+            result = prev * current;
+            break;
+        case "/":
+            if(current === 0){
+                result = "ERROR";
+            }
+            else{
+                result = prev / current;
+            }
+            break;
+        default:
+            return "ERROR";
+    }
+    display.textContent = result.toString().slice(0, 11);
+    currentOperand = result;
+    operator = null;
 }
-
-var clearButton = document.querySelector(".clear");
-var clear = document.createElement("button");
-clear.innerText = "CE";
-clearButton.appendChild(clear);
